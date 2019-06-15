4326ddee4d371f23d779e4d89fcc5ad0840151df
vadym.b <vadimberkut8@gmail.com>
2019-06-15 22:09:20 +0300

    add webpack; add build, watch scripts; add example

---
+++ Lexiconner/Lexiconner.Web/ClientVanilaJs/utils/exampleUtil.js
@@ -0,0 +1,16 @@
+﻿
+class ExampleUtil {
+    constructor() {
+
+    }
+
+    static testStatic(message) {
+        console.log(message);
+    }
+
+    test(message) {
+        console.log(message);
+    }
+}
+
+export default ExampleUtil;
