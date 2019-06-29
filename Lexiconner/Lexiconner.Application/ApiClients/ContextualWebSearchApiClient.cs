aaaf2ff109feb41ceba8b96834baacd1f2b6f9b5
vadym.b <vadimberkut8@gmail.com>
2019-06-29 12:36:48 +0300

    add draft for api clients

---
+++ Lexiconner/Lexiconner.Application/ApiClients/ContextualWebSearchApiClient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lexiconner.Application.ApiClients
+{
+    public class ContextualWebSearchApiClient
+    {
+    }
+}
---
+++ Lexiconner/Lexiconner.Application/ApiClients/GoogleTranslateApiClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lexiconner.Application.ApiClients
+{
+    public interface IGoogleTranslateApiClient
+    {
+
+    }
+
+    /// <summary>
+    /// The free tier is only available for Translation API v3.
+    /// </summary>
+    public class GoogleTranslateApiClient : IGoogleTranslateApiClient
+    {
+        private const int FreeCharactersLimitV3 = 500_000;
+
+        public async Task<string> Translate(string text, string language)
+        {
+            return "";
+        }
+    }
+}
