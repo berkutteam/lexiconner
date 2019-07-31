d67460975c77a185e8c17fcd056051d394c2373d
vadimberkut <vadimberkut8@gmail.com>
2019-07-31 16:32:33 +0300

    add training models

--- Lexiconner/Lexiconner.Domain/Entitites/StudyItemEntity.cs
+++ Lexiconner/Lexiconner.Domain/Entitites/StudyItemEntity.cs
@@ -1,6 +1,10 @@
 ﻿using Lexiconner.Domain.Entitites.Base;
+using Lexiconner.Domain.Enums;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Lexiconner.Domain.Entitites
@@ -17,9 +21,12 @@ namespace Lexiconner.Domain.Entitites
         public string Description { get; set; }
         public string ExampleText { get; set; }
         // public string ExampleImageUrl { get; set; }
-        public StudyItemImageEntity Image { get; set; }
 
         public List<string> Tags { get; set; }
+
+        public StudyItemImageEntity Image { get; set; }
+        public StudyItemTrainingInfoEntity TrainingInfo { get; set; }
+
     }
 
     public class StudyItemImageEntity : BaseEntity
@@ -31,5 +38,36 @@ namespace Lexiconner.Domain.Entitites
         public string ThumbnailHeight { get; set; }
         public string ThumbnailWidth { get; set; }
         public string Base64Encoding { get; set; }
+    }
+
+    public class StudyItemTrainingInfoEntity : BaseEntity
+    {
+        public StudyItemTrainingInfoEntity()
+        {
+            Trainings = new List<StudyItemTrainingProgressItemEntity>();
+        }
+
+        public List<StudyItemTrainingProgressItemEntity> Trainings { get; set; }
+
+        public decimal GetOverallProgress()
+        {
+            var progress = Math.Round(Trainings.Select(x => x.Progress).Sum() / Trainings.Count, 2);
+            return progress;
+        }
+
+
+
+        public class StudyItemTrainingProgressItemEntity : BaseEntity
+        {
+            [JsonConverter(typeof(StringEnumConverter))]
+            public TrainingType TrainingType { get; set; }
+            public DateTime? LastTrainingdAt { get; set; }
+            public DateTime? NextTrainingdAt { get; set; }
+
+            /// <summary>
+            /// Percents [0, 1]
+            /// </summary>
+            public decimal Progress { get; set; }
+        }
     }
 }
