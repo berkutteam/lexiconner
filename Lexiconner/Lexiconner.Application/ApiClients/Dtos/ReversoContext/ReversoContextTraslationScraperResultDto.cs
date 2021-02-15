bee1213634a6240ed83408154f1afa43db6bf8a0
vadimberkut <vadimberkut8@gmail.com>
2021-02-15 15:27:59 +0200

    add ReversoContextScraper

---
+++ Lexiconner/Lexiconner.Application/ApiClients/Dtos/ReversoContext/ReversoContextTraslationScraperResultDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lexiconner.Application.ApiClients.Dtos.ReversoContext
+{
+    public class ReversoContextTraslationScraperResultDto
+    {
+        public ReversoContextTraslationScraperResultDto()
+        {
+            Results = new List<ReversoContextTraslationScraperResultItemDto>();
+        }
+
+        /// <summary>
+        /// E.g. english
+        /// </summary>
+        public string SourceLanguage { get; set; }
+
+        /// <summary>
+        /// E.g. en
+        /// </summary>
+        public string SourceLanguageCode { get; set; }
+        public string TargetLanguage { get; set; }
+        public string TargetLanguageCode { get; set; }
+        public string SearchedWord { get; set; }
+        public IList<ReversoContextTraslationScraperResultItemDto> Results { get; set; }
+    }
+
+    public class ReversoContextTraslationScraperResultItemDto
+    {
+        public string SourceLanguageSentence { get; set; }
+        public string TargetLanguageSentence { get; set; }
+    }
+}
---
+++ Lexiconner/Lexiconner.Domain/Dtos/Words/WordExamplesDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lexiconner.Domain.Dtos.Words
+{
+    public class WordExamplesDto
+    {
+        public WordExamplesDto()
+        {
+            Examples = new List<string>();
+        }
+
+        public string Word { get; set; }
+        public string LanguageCode { get; set; }
+        public IEnumerable<string> Examples { get; set; }
+    }
+}
